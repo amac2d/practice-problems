1f0b713cfe2fba77ade28be02d0f07337ffe0d0e
Alexander Mac <amac2@student.mtsac.edu>
2019-05-21 09:41:00 -0700

    Drills01 with 2 finished

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,11 +1,28 @@
 
-function sumArray(){
+function sumArray(array){
+    var sumOfArray = 0;
+    for (var indexArray = 0 ; indexArray < array.length ; indexArray++) {
+        sumOfArray += array[indexArray];
+    }
+    return sumOfArray;
+}
+
+function fitWithinVal(array, num){
+    var sumOfArray2 = 0;
+    var arrayReturn = [];
+    for (var indexArray2 = 0; indexArray2 < array.length; indexArray2++) {
+        if (array[indexArray2] + sumOfArray2 <= num) {
+            sumOfArray2 += array[indexArray2];
+            arrayReturn.push(array[indexArray2]);
+        }
+        else if (sumOfArray2 === num){
+            indexArray2 = array.length
+        }
+    }
+    return arrayReturn;
 
 }
 
-function fitWithinVal(){
-
-}
 
 function getAllNamesShorterThan(){
 

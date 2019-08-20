05fe2ede12f0ffeecd0440776ad5758c81278f04
Alexander Mac <amac2@student.mtsac.edu>
2019-08-19 23:53:41 -0700

    still need to make populate the data

--- drills05/yourcode.js
+++ drills05/yourcode.js
@@ -1,7 +1,19 @@
 
 
-function populateRecords(){
-
+function populateRecords( arr ){
+  let obj = {
+      charge: 0,
+      'cash advance': 0
+  };
+  for ( let index = 0; index < arr.length; index++){
+      if(arr[index].type === 'charge'){
+          obj.charge += parseFloat(arr[index].amount);
+      } else {
+          obj['cash advance'] += parseFloat(arr[index].amount);
+      }
+      
+  }
+  return obj;
 }
 
 
